a71b7516c81dad90d162cecdcbc1f3ee2fc1e503
Serranch <120034702@upq.edu.mx>
2022-11-30 15:02:19 -0600

    Update controladorBDCli.php

--- app/Http/Controllers/controladorBDCli.php
+++ app/Http/Controllers/controladorBDCli.php
@@ -30,7 +30,7 @@ class controladorBDCli extends Controller
      */
     public function create()
     {
-        return view('formularioCli')
+        return view('formularioCli');
     }
 
     /**
